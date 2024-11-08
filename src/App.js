d876bb0a4ea4fbf8f093011e2628f028535ce7eb
Drake <sumitdrak@gmail.com>
2024-11-08 19:54:04 +0530

    navigation done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import react from "react";
-import Header from "./components/Header";
-import Todoitems from "./components/Todoitems";
-import Addbutton from "./components/Addbutton";
-import "./style.css";
 
 function App() {
-  return (
-    <div className="todo-container">
-      <h1>Project</h1>
-    </div>
-  );
+  return <>
+    
+  </>
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import react from "react";
+import "../App.css";
+import brandLogo from "../images/brand_logo.png";
+
+function Navigation() {
+  return (
+    <div className="nav-bar">
+      <nav>
+        <div className="logo">
+          <img src={brandLogo} alt="brand_logo"></img>
+        </div>
+
+        <ul>
+          <li href="#"> MENU</li>
+          <li href="#"> LOCATION</li>
+          <li href="#"> ABOUT</li>
+          <li href="#"> CONTACT</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
