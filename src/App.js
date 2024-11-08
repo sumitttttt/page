818f5a8cdd12a41f81eb217080ba355f762a61cf
Drake <sumitdrak@gmail.com>
2024-11-08 19:13:09 +0530

    Initial build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import Header from "./components/Header";
+import Todoitems from "./components/Todoitems";
+import Addbutton from "./components/Addbutton";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+      <h1>Project</h1>
     </div>
   );
 }
